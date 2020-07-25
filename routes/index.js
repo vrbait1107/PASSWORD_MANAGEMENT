2a1314f3537a21ca5561a0a092153a6dd73283c3
Vishal_Bait <vishalbait01@gmail.com>
2020-07-25 17:16:18 +0530

    Add register, login addNewCategory, passwordCategory files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Password Management System" });
+});
+
+router.get("/register", (req, res, next) => {
+  res.render("register", { title: "Password Management System" });
+});
+
+router.get("/passwordCategory", (req, res, next) => {
+  res.render("passwordCategory", { title: "Password Details" });
+});
+
+router.get("/addNewCategory", (req, res, next) => {
+  res.render("addNewCategory", { title: "Password Management Details" });
 });
 
 module.exports = router;
