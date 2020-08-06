84b018c749d631a850728f778282e2acadf4965b
Vishal_Bait <vishalbait01@gmail.com>
2020-08-06 18:47:46 +0530

    Add two pages addNewPassword, viewAllPassword addNewPassword for adding new password viewAllPassword for view all password

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,12 @@ router.get("/addNewCategory", (req, res, next) => {
   res.render("addNewCategory", { title: "Password Management Details" });
 });
 
+router.get("/addNewPassword", (req, res, next) => {
+  res.render("addNewPassword", { title: "Password Management Details" });
+});
+
+router.get("/viewAllPassword", (req, res, next) => {
+  res.render("viewAllPassword", { title: "Password Management Details" });
+});
+
 module.exports = router;
